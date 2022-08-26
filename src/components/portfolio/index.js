04f2d8080f1004abc94cc24bceaa69e37aa5077b
Scott Moore <sm91srjc@gmail.com>
2022-08-25 23:22:58 -0700

    work section

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -4,33 +4,31 @@ import sphereImage from "../../assets/img/sphere.png";
 function Portfolio() {
   return (
     <div>
-      <section id="work" className="">
+      <section id="" className="sphere-section">
         <div className="flex-row">
-          <h2 className="">Work</h2>
+          <h2 className="Work">Work</h2>
         </div>
 
-        <div className="">
-          <div className="">
-            <div className="">
-              <a href="https://github.com/Lemming97/Project_3">
+          <div className="group-sphere">
+              
+            <div className="sphere-text">
+              <div className="sphere-title">The Sorcerer's Sphere</div>
+              <p>
+                Believe in the unknown? Take a trip to The Sorcerer's Sphere!
+                A fully immersive where you can ask the sphere for guidance,
+                pay a trip to magic mark for life advice, get you lucky lotto
+                numbers, and if you're feeling generous donate a dollar.
+              </p>
+            </div>
+            <a href="https://github.com/Lemming97/Project_3">
                 {" "}
-                <img
+                <img 
                   src={sphereImage}
-                  className=""
-                  style={{ width: "50%" }}
+                  className="sphere-pic"
                   alt=""
                 />
               </a>
-            </div>
-            <div className="job-text">
-              <h4>The Sorcerer's Sphere</h4>
-              <p>
-                A Carnival style application that lets users get fortunes, lucky
-                numbers, affirmations, and enjoyment.
-              </p>
-            </div>
           </div>
-        </div>
       </section>
     </div>
   );
